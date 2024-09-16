e22b30247cd9700a1a95634a3f31a8bca5320634
Ehab-24 <ehabs1775@gmail.com>
2024-09-16 23:10:31 +0500

    feat: add multi-language support
    Add support for:
    - English
    - Urdu

---
+++ src/i18n/en.js
@@ -0,0 +1,56 @@
+const en_i18n_data = {
+    "title": "Ehab Sohail",
+    "welcome_message": "Welcome to the website of yours truly!",
+    "about_me": "Learn more about me and my life.",
+    "footer_text": "© 2023 Ehab Sohail. All rights reserved.",
+
+    "nav_link_ur": "Urdu",
+    "nav_link_en": "English",
+    "nav_link_projects": "Projects",
+    "nav_link_education": "Education",
+    "nav_link_experience": "Experience",
+
+    "projects_heading": "Projects",
+
+    "project_portfolio_heading": "Portfolio",
+    "project_sohaib_wahab_llc_heading": "Sohaib Wahab LLC",
+    "project_zeevista_heading": "Zeevista",
+    "project_zavian_consultancy_heading": "Zavian Consultancy",
+    "project_ultimate_heading_editor_text": "Ultimate Text Editor",
+    "project_cisgroup_heading": "Cisgroup",
+    "project_algorithm_visualizer_heading": "Algorithm Visualizer",
+    "project_scribe_heading": "Scribe",
+
+    "project_portfolio_text": "What do you do when you are a student who is over confident in his knowledge? You create a Portfolio! and showcase it to the world with the expectation of praise ✨.",
+    "project_sohaib_wahab_llc_text": "Sohaibwahabllc is a custom e-commerce for a local client in Pakistan. I built their web app in Sveltekit.",
+    "project_zeevista_text": "Zeevista is a UAE based consultancy immigration advisor firm. We built their web application in reactjs",
+    "project_zavian_consultancy_text": "Zavian Consultancy is a UAE based firm that provides consultancy on immigration for students, job seekers and visitors. I built their website in ReactJS.",
+    "project_ultimate_text_editor_text": "Ultimate Text Editor is a CLI based text editor that supports multiple tabs, find and replace, text highlighting and even an in-built mini game.",
+    "project_cisgroup_text": "Cisgroup is a UAE based consultancy firm that provides migration consultancy services. We built their website in ReactJS.",
+    "project_algorithm_visualizer_text": "Algorithm Visualizer is a web application written in flutter. The app aims to help understand complex path-finding and sorting algorithms visually. Supported algorithms include: Dijkstra's algo, BFS, DFS, A-Star Search, Best First Search, and Cocktail Sort, Quick Sort, Heap Sort, Selection Sort, Insertion Sort and Bubble Sort",
+    "project_scribe_text": "Scribe is a mobile application written in flutter. The aim of this app is to assist readers to easily search for word definitions, meanings and synonyms, and track their history for self analysis.",
+
+    "education_heading": "Education",
+
+    "education_cs_degree": "BACHELORS IN COMPUTER SCIENCE",
+    "education_cs_institution": "INFORMATION TECHNOLOGY UNIVERSITY",
+    "education_cs_date_range": "Aug 2021 - May 2025",
+
+    "education_intermediate_degree": "INTERMEDIATE IN PRE-ENGINEERING",
+    "education_intermediate_institution": "CRESCENT MODEL HIGHER SECONDARY SCHOOL",
+    "education_intermediate_date_range": "Sep 2019 - Jun 2021",
+
+    "experience_heading": "Experience",
+
+    "experience_insivalley_title": "Software Engineer",
+    "experience_insivalley_organization": "﹫Insivalley",
+    "experience_insivalley_date_range": "Mar 2024 - Current",
+
+    "experience_bidbazaar_title": "Web Developer",
+    "experience_bidbazaar_organization": "﹫Bidbazaar",
+    "experience_bidbazaar_date_range": "Jun 2023 - Oct 2023",
+
+    "social_feed_heading": "Social Feed",
+
+    "placeholder_text": "This sentence is merely a placeholder, and although it has no meaning, it still provides value. These are the things that, when used at the right place, can create an impact wothout leaving a mark."
+}
---
+++ src/i18n/ur.js
@@ -0,0 +1,55 @@
+const ur_i18n_data = {
+    "title": "ایہاب سہیل",
+    "welcome_message": "میری ویب سائٹ پر خوش آمدید!",
+    "about_me": "میرے اور میری زندگی کے بارے میں مزید جانیں۔",
+    "footer_text": "© ۲۰۲۳ جملہ حقوق محفوظ ہیں۔ ایہاب سہیل۔",
+
+    "nav_link_ur": "اردو",
+    "nav_link_en": "انگریزی",
+    "nav_link_projects": "منصوبے",
+    "nav_link_education": "تعلیم",
+    "nav_link_experience": "تجربہ",
+
+    "projects_heading": "منصوبے",
+
+    "project_portfolio_heading": "پورٹ فولیو",
+    "project_sohaib_wahab_llc_heading": "صہیب وہاب ایل ایل سی",
+    "project_zeevista_heading": "زیویسٹا۔",
+    "project_zavian_consultancy_heading": "زاویان کنسلٹنسی",
+    "project_ultimate_text_editor_heading": "الٹیمیٹ ٹیکسٹ ایڈیٹر",
+    "project_cisgroup_heading": "سِس گروپ",
+    "project_algorithm_visualizer_heading": "الگورتھم ویژولائزر",
+    "project_scribe_heading": "سکرائب",
+
+    "project_portfolio_text": "اگر آپ ایک طالب علم ہوں جو اپنے علم پر زیادہ اعتماد رکھتا ہو تو آپ کیا کریں گے؟ آپ ایک پورٹ فولیو بنائے گے! اور تعریف کی توقع کے ساتھ اسے دنیا کے سامنے پیش کریں گے ✨۔",
+    "project_sohaib_wahab_llc_text": "صہیب وہاب ایل ایل سی پاکستان میں مقامی کلائنٹ کے لیے ایک حسب ضرورت ای کامرس پلیٹ فارم ہے۔ میں نے ان کی ویب ایپ سویلٹکٹ میں بنائی تھی۔",
+    "project_zeevista_text": "زیویسٹا۔ متحدہ عرب امارات میں قائم کنسلٹنسی امیگریشن ایڈوائزر فرم ہے۔ ہم نے ان کی ویب ایپلیکیشن \"ریایکٹ ج س\" میں بنائی تھی۔",
+    "project_zavian_consultancy_text": "زاویان کنسلٹنسی. متحدہ عرب امارات کی ایک فرم ہے جو طلباء، ملازمت کے متلاشیوں اور زائرین کے لیے امیگریشن پر مشاورت فراہم کرتی ہے۔ میں نے ان کی ویب سائٹ \"ری ایکٹ جے ایس\" میں بنائی۔",
+    "project_ultimate_text_editor_text": "االٹیمیٹ ٹیکسٹ ایڈیٹرلٹیمیٹ ٹیکسٹ ایڈیٹر",
+    "project_cisgroup_text": " سیسگروپ متحدہ عرب امارات میں مقیم کنسلٹنسی فرم ہے جو مائیگریشن کنسلٹنسی کی خدمات فراہم کرتی ہے۔ ہم نے ان کی ویب سائٹ \"ری ایکٹ جے ایس\" میں بنائی ہے۔",
+    "project_algorithm_visualizer_text": "ایلگوریتھم ویزوالایزر ایک ویب ایپلیکیشن ہے جو فلٹر میں لکھی گئی ہے۔ ایپ کا مقصد پیچیدہ راستے تلاش کرنے اور الگورتھم کو بصری طور پر ترتیب دینے میں مدد کرنا ہے۔ تائید شدہ الگورتھم میں شامل ہیں۔",
+    "project_scribe_text": "سکرائب ایک موبائل ایپلی کیشن ہے جو پھڑپھڑاہٹ میں لکھی جاتی ہے۔ اس ایپ کا مقصد قارئین کو آسانی سے الفاظ کی تعریف، معنی اور مترادفات تلاش کرنے میں مدد کرنا ہے اور خود تجزیہ کے لیے ان کی تاریخ کو ٹریک کرنا ہے۔",
+
+    "education_heading": "تعلیم",
+    "education_cs_degree": "کمپیوٹر سائنس میں بیچلرز",
+    "education_cs_institution": "انفارمیشن ٹیکنالوجی یونیورسٹی",
+    "education_cs_date_range": "اگست ۲۰۲۱ - مئ ۲۰۲۵",
+
+    "education_intermediate_degree": "پری انجینئرنگ میں انٹرمیڈیٹ",
+    "education_intermediate_institution": "کریسنٹ ماڈل ہائیر سیکنڈری سکول",
+    "education_intermediate_date_range": "ستمبر ۲۰۱۹ - جون ۲۰۲۱",
+
+    "experience_heading": "تجربہ",
+
+    "experience_insivalley_title": "سافٹ ویئر انجینئر",
+    "experience_insivalley_organization": "﹫اِنسائویلی",
+    "experience_insivalley_date_range": "مارچ ۲۰۲۳ - موجودہ",
+
+    "experience_bidbazaar_title": "ویب ڈویلپر",
+    "experience_bidbazaar_organization": "﹫بِڈبازار",
+    "experience_bidbazaar_date_range": "جون ۲۰۲۳ . نومبر ۲۰۲۳",
+
+    "social_feed_heading": "سماجی فیڈ",
+
+    "placeholder_text": "یہ جملہ محض ایک پلیس ہولڈر ہے، اور اگرچہ اس کا کوئی مطلب نہیں، پھر بھی یہ قدر فراہم کرتا ہے۔ یہ وہ چیزیں ہیں جن کا صحیح جگہ پر استعمال کیا جائے تو کوئی نشان چھوڑے بغیر اثر پیدا کر سکتا ہے۔"
+}
